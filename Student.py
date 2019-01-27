cebd40c1752d3e88eac407b3e93bc56e8c5267ca
Siddhartha <sidb91@outlook.com>
2019-01-27 09:52:54 +0530

    Getting started with Python - initial set of script files

---
+++ Student.py
@@ -0,0 +1,5 @@
+class Student: #Student data type
+    #initialize function - values to represent a Student
+    def __init__(self,name, major, gpa, is_on_probation):
+        self.name = name
+        self
---
+++ max_of_2_numbers.py
@@ -0,0 +1,10 @@
+def maximum_of_numbers():
+    x = int(input("enter first number: "))
+    y = int(input("enter second number: "))
+
+    if x>y:
+        print(x)
+    else:
+        print(y)
+
+maximum_of_numbers()
